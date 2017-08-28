10bf95810a55f9f8e37b4ae80421a7d243db9f1a
Dvd <dzung.dvd@gmail.com>
2017-08-28 09:17:50 +0700

    Scene 4 & 5

---
+++ Session 9/God Tower/Assets/Scripts/HintChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintChanger : MonoBehaviour {
+
+	public List<GameObject> hintsList;
+	public Button hintButton;
+
+	int hintCurrentIndex = 0;
+
+	public void OnHintButtonClick()
+	{
+		hintCurrentIndex += 1;
+
+		if (hintCurrentIndex == hintsList.Count - 1) {
+			hintButton.transform.localScale = new Vector3 (-1, 1, 1);
+		} else {
+			hintButton.transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (hintCurrentIndex == hintsList.Count) {
+			hintCurrentIndex = 0;
+		}
+
+		for (int i = 0; i < hintsList.Count; i++) {
+			if (i == hintCurrentIndex) {
+				hintsList [i].gameObject.SetActive (true);
+			} else {
+				hintsList [i].gameObject.SetActive (false);
+			}
+		}
+		
+	}
+
+}
