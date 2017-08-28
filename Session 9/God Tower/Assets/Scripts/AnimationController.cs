486765b343740b620bb81e1caec314808b2568ed
Asus Laptop <dzung.dvd@gmail.com>
2017-08-28 22:58:56 +0700

    Session 11 class

---
+++ Session 9/God Tower/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	public float moveSpeed = 2f;
+	public float distance = 10f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (MoveRight ());
+	}
+
+	IEnumerator MoveRight()
+	{
+		float distanceMoved = 0f;
+		while (distanceMoved < distance) {
+			transform.position = new Vector3(
+				transform.position.x + moveSpeed * Time.deltaTime,
+				transform.position.y, 
+				transform.position.z
+			);
+			distanceMoved += moveSpeed * Time.deltaTime;
+			yield return null;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.B)) {
+			Debug.Log("B pressed");
+		}
+	}
+}
--- Session 9/God Tower/Assets/Scripts/Level5Controller.cs
+++ Session 9/God Tower/Assets/Scripts/Level5Controller.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Level5Controller : EventTrigger {
 
-	public override void OnBeginDrag(PointerEventData data){
-		
+	Image thisImage;
+	Vector3 offset;
+
+	void Start(){
+		thisImage = GetComponent<Image> ();
+		float x = Random.Range (-220f, 220f);
+		float y = Random.Range (-195f, 195f);
+		transform.localPosition = new Vector3 (x,y,transform.localPosition.z);
+		thisImage.alphaHitTestMinimumThreshold = 0.5f;
 	}
 
-	public override void OnDrag(PointerEventData data){
+	public override void OnBeginDrag(PointerEventData data){
 		Vector3 screenMousePos = Input.mousePosition;
 		Vector3 worldSpaceMousePos = Camera.main.ScreenToWorldPoint (screenMousePos);
 		Vector3 canvasMousePos = transform.parent.InverseTransformPoint (worldSpaceMousePos);
-		transform.localPosition = new Vector3 (canvasMousePos.x, canvasMousePos.y, transform.localPosition.z);
+
+		offset = canvasMousePos - transform.localPosition;
+	}
+
+	public override void OnDrag(PointerEventData data){
+		//get mouse position in canvas
+		Vector3 screenMousePos = Input.mousePosition;
+		Vector3 worldSpaceMousePos = Camera.main.ScreenToWorldPoint (screenMousePos);
+		Vector3 canvasMousePos = transform.parent.InverseTransformPoint (worldSpaceMousePos);
+
+		//image position without clamp
+		Vector3 imagePos = canvasMousePos - offset;
+
+		//clamp image to a zone
+		float clampedImagePosX = Mathf.Clamp (imagePos.x, -220f, 220f);
+		float clampedImagePosY = Mathf.Clamp (imagePos.y, -220f, 220f);
+
+		//move the image
+		transform.localPosition = new Vector3 (clampedImagePosX, clampedImagePosY, transform.localPosition.z);
 	}
 }
