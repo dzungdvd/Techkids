ad4ee3328925663e778defce261d226c1514b644
Dvd <dzung.dvd@gmail.com>
2017-10-06 14:23:45 +0700

    frontier2d

---
+++ Frontier2D/Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject target;
+	public float smoothing = 8f;
+
+	void Start()
+		{
+			target = GameObject.FindGameObjectWithTag("Player");
+		}
+		
+
+	void FixedUpdate () {
+		if (target == null)
+		{
+			target = GameObject.FindGameObjectWithTag("Player");
+		}
+		else
+		{
+			Vector2 targetCamPos = target.transform.position;
+			transform.position = Vector2.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+		}
+	}
+}
